345e91d623a0af3019281eb52b00e50dfeccb99f
RISHABH didz <RISHABH didz>
2016-08-14 15:46:22 +0530

    klot

---
+++ sem5/scripts/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+	$(".include-navbar-common").load("sem3/pages/important/navbar-common.html");
+	$(".include-navbar-subject").load("sem3/pages/important/navbar-subject.html");
+});
