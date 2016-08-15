82936a6e303504c7c58a31c6580581af308ce78e
rakshitha05 <rakshitha.gk@gmail.com>
2016-08-15 17:57:52 +0530

    sem1

---
+++ sem1/scripts/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+	$(".include-navbar-common").load("sem1/pages/important/navbar-common.html");
+	$(".include-navbar-subject").load("sem1/pages/important/navbar-subject.html");
+});
