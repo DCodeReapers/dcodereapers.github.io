d11d6b41204a2654bfd80767c38c2c0c23b3c5b7
harshit arun <harshit arun>
2016-08-19 20:41:35 +0530

    sem1

--- sem1/scripts/main.js
+++ sem1/scripts/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
 	$(".include-navbar-common").load("sem1/pages/important/navbar-common.html");
 	$(".include-navbar-subject").load("sem1/pages/important/navbar-subject.html");
+	$(".footer").load("sem1/pages/important/footer.html");
 });
