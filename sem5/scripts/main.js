fcac748efc20b87f801042d35829f83ef12959dc
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2016-08-20 15:07:37 +0530

    sem5 by sujay

--- sem5/scripts/main.js
+++ sem5/scripts/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
 	$(".include-navbar-common").load("sem5/pages/important/navbar-common.html");
 	$(".include-navbar-subject").load("sem5/pages/important/navbar-subject.html");
-	$(".footer").load("sem5/pages/important/footer.html");
 });
