56fc83650cb3abf194baf2278838a941601de5b8
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2017-03-15 01:29:38 +0530

    sem 2 added by shashank

---
+++ sem2/scripts/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	$(".include-navbar-common").load("sem2/pages/important/navbar-common.html");
+	$(".include-navbar-subject").load("sem2/pages/important/navbar-subject.html");
+	$(".footer").load("sem2/pages/important/footer.html");
+});
